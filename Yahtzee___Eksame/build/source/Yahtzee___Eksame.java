e36d9354668a2840dac1ff979c0573326f4ce8a1
Fred993d <37065615+Fred993d@users.noreply.github.com>
2020-01-09 12:54:04 +0100

    stavefejl

--- Yahtzee___Eksame/build/source/Yahtzee___Eksame.java
+++ Yahtzee___Eksame/build/source/Yahtzee___Eksame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Yahtzee___Eksame extends PApplet {
+
 
 
 
@@ -59,7 +75,7 @@ int w6 = 50;
 int h6 = 50;
 
 
-//tredje linje
+//tjreje linje
 
 int x7 = 950;
 int y7 = 300;
@@ -81,14 +97,14 @@ int h9 = 50;
 
 
 
-void setup() {
-  size(1200,950);
+public void setup() {
+  
 
 }
 
 
 
-void draw() {
+public void draw() {
 
 
 
@@ -104,7 +120,7 @@ void draw() {
 
 //rect(x.y,width,height);
 
-//tom/you/opponent
+//tom/you/opponet
 rect(900, 50, 100, 50);
 rect(1000, 50, 50, 50);
 rect(1050, 50, 50, 50);
@@ -119,7 +135,7 @@ rect(900, 150, 100, 50);
 rect(1000, 150, 50, 50);
 rect(1050, 150, 50, 50);
 
-//Threes
+//Threees
 rect(900, 200, 100, 50);
 rect(1000, 200, 50, 50);
 rect(1050, 200, 50, 50);
@@ -135,6 +151,7 @@ rect(1000, 300, 50, 50);
 rect(1050, 300, 50, 50);
 
 //Sixes
+
 rect(900, 350, 100, 50);
 rect(1000, 350, 50, 50);
 rect(1050, 350, 50, 50);
@@ -147,6 +164,7 @@ rect(1050, 400, 50, 50);
 
 
 // Bonus
+
 rect(900,  450, 100, 50);
 rect(1000, 450, 50, 50);
 rect(1050, 450, 50, 50);
@@ -157,11 +175,12 @@ rect(1000, 500, 50, 50);
 rect(1050, 500, 50, 50);
 
 // Four of a kind
+
 rect(900,  550, 100, 50);
 rect(1000, 550, 50, 50);
 rect(1050, 550, 50, 50);
 
-// Full House
+// Full Haouse
 rect(900,  600, 100, 50);
 rect(1000, 600, 50, 50);
 rect(1050, 600, 50, 50);
@@ -193,6 +212,7 @@ rect(1050, 800, 50, 50);
 
 
 // Total Score
+
 rect(900,  850, 100, 50);
 rect(1000, 850, 50, 50);
 rect(1050, 850, 50, 50);
@@ -237,7 +257,7 @@ text("TOTAL SCORE", 910,880);
 
 
 
- //de øverste
+ //de øverester
  if ((mouseX>x1 && mouseX <x1+w1 && mouseY>y1 && mouseY <y1+h1)){
 
     fill (255,0,0);
@@ -281,7 +301,7 @@ if ((mouseX>x2 && mouseX <x2+w2 && mouseY>y2 && mouseY <y2+h2)){
 
  }
 
-    //anden række
+    //andne række
 
      if ((mouseX>x4 && mouseX <x4+w4 && mouseY>y4 && mouseY <y4+h4)){
 
@@ -308,7 +328,7 @@ if ((mouseX>x5 && mouseX <x5+w5 && mouseY>y5 && mouseY <y5+h5)){
 
 
 
-      // tredje række
+      // tjrene række
 
      if ((mouseX>x7 && mouseX <x7+w7 && mouseY>y7 && mouseY <y7+h7)){
 
@@ -338,4 +358,14 @@ if ((mouseX>x8 && mouseX <x8+w8 && mouseY>y8 && mouseY <y8+h8)){
 
 
 
+}
+  public void settings() {  size(1200,950); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "Yahtzee___Eksame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
